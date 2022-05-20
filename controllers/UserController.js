adb40b3e521492817d312287c873056e510be36f
nhadi44 <hadinurhidayat97@hotmail.com>
2022-05-20 22:05:55 +0700

    finish crud

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -69,4 +69,69 @@ const getUserByEmail = async (req, res) => {
     console.log(error);
   }
 };
-module.exports = { getAll, getOne, getUserByEmail };
+
+const updateUser = async (req, res) => {
+  const id = req.params.id;
+  const { name, email } = req.body;
+
+  try {
+    const updateUser = await UserModel.update(
+      {
+        name: name,
+        email: email,
+      },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+
+    const user = await UserModel.findOne({
+      attributes: ["id", "name", "email", "createdAt", "updatedAt"],
+      where: {
+        id: id,
+      },
+    });
+
+    if (updateUser[0] === 0)
+      return res.status(404).json({
+        status: "error",
+        message: "User not found",
+      });
+
+    return res.json({
+      status: "success",
+      message: "User updated successfully",
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const deleteUser = await UserModel.destroy({
+      where: {
+        id: id,
+      },
+    });
+
+    if (deleteUser === 0)
+      return res.status(404).json({
+        status: "error",
+        message: "User not found",
+      });
+
+    return res.json({
+      status: "success",
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = { getAll, getOne, getUserByEmail, updateUser, deleteUser };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const userRoute = express.Router();
+const UserModel = require("../models").User;
+const { check } = require("express-validator");
+const validation = require("../middleware/ValidationMiddleware");
 
 const {
   getAll,
   getOne,
   getUserByEmail,
+  updateUser,
+  deleteUser,
 } = require("../controllers/UserController");
 
 userRoute.get("/", getAll);
 userRoute.get("/:id", getOne);
 userRoute.get("/email/:email", getUserByEmail);
+userRoute.put(
+  "/:id/update",
+  check("name").isLength({ min: 1 }).withMessage("Name is requried"),
+  check("email")
+    .isEmail()
+    .custom((value) => {
+      return UserModel.findOne({ where: { email: value } }).then((user) => {
+        if (user) {
+          return Promise.reject("Email already exists");
+        }
+      });
+    }),
+  validation,
+  updateUser
+);
+
+userRoute.delete("/:id/delete", deleteUser);
 
 module.exports = userRoute;
